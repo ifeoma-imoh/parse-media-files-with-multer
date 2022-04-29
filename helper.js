33973710bf3d266a4aaab6cf51d65bcfe5d1a261
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-04-29 15:52:53 +0400

    Updated helper file

--- helper.js
+++ helper.js
@@ -1,8 +1,8 @@
 const cloudinary = require("cloudinary").v2;
 cloudinary.config({
-  cloud_name:process.env.REACT_APP_CLOUD_NAME,
-  api_key:process.env.REACT_APP_API_KEY,
-  api_secret:process.env.REACT_APP_API_SECRET
+  cloud_name:process.env.CLOUD_NAME,
+  api_key:process.env.API_KEY,
+  api_secret:process.env.API_SECRET
 });
 export async function handleUpload(file) {
   const res = await cloudinary.uploader.upload(file, {
