81347974b01b229cf259e034e6a328b8a93c1c4d
ifeoma-imoh <imoh_ifeoma@yahoo.co.uk>
2022-05-02 17:27:03 +0400

    debugging vercel

--- helper.js
+++ helper.js
@@ -5,6 +5,7 @@ cloudinary.config({
   api_secret:process.env.API_SECRET
 });
 export async function handleUpload(file) {
+  console.log(process.env.API_KEY, process.env.API_SECRET, process.env.CLOUD_NAME )
   const res = await cloudinary.uploader.upload(file, {
     resource_type: "auto"
   });
